411620d912e2290355a038db5ea51679c001e1b6
vinicius@digitalhouse.com <vinicius@digitalhouse.com>
2019-04-30 19:49:34 +0000

    Mumuki Export on 2019-04-30 19:49:34 +0000

--- 00001_Minha primeira váriavel/test.php
+++ 00001_Minha primeira váriavel/test.php
@@ -1,11 +1,11 @@
 public function teste(): void {
 
   /*...content...*/
-  $sol = '/*...content...*/';
+  $solucaoAluno = '/*...content...*/';
   
   $this->assertTrue(isset($first), 'A váriavel $first não está definida!');
   
-  $haveEcho = strpos($sol, 'echo $first');
+  $haveEcho = strpos($solucaoAluno, 'echo $first');
   
   $this->assertTrue($haveEcho !== false, "Não foi feita a impressão do valor da váriavel usando o echo!");
 }

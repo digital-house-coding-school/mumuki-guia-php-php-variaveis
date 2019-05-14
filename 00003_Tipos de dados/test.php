c8c28bded7f631af38a6ee81a433f429b5688549
vtorres@digitalhouse.com <vtorres@digitalhouse.com>
2019-05-14 22:07:09 +0000

    Mumuki Export on 2019-05-14 22:07:09 +0000

--- 00003_Tipos de dados/test.php
+++ 00003_Tipos de dados/test.php
@@ -9,11 +9,11 @@ public function testDescriptionExample(): void {
   $this->assertTrue(isset($numeroInteiro), 'A váriavel $numeroInteiro não está definida.');
   $this->assertTrue(isset($numeroDecimal), 'A váriavel $numeroDecimal não está definida.');
   $this->assertTrue(isset($verdade), 'A váriavel $verdade não está definida.');
-  $this->assertTrue($nulo, 'A váriavel $nulo não está definida.');
+  $this->assertTrue(isset($nulo), 'A váriavel $nulo não está definida.');
   
   $this->assertTrue(gettype($texto) == "string", 'A váriavel $texto deve ser um tipo String');
   $this->assertTrue(gettype($numeroInteiro) == "integer", 'A váriavel $numeroInteiro deve ser um tipo Inteiro');
   $this->assertTrue(gettype($numeroDecimal) == "double", 'A váriavel $numeroDecimal deve ser um tipo Decimal');
   $this->assertTrue(gettype($verdade) == "boolean", 'A váriavel $verdade deve ser um tipo Boolean');
-  $this->assertTrue(is_null($nulo), 'A váriavel $nulo deve ser um tipo Null');
+  $this->assertTrue(is_null($nulo), 'A váriavel $nulo deve ser um tipo null');
 }

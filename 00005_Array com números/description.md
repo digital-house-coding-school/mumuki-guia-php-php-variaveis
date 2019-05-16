bd9d3f3cc5ab210f57ba552478c0bdcfb1c36072
vinicius@digitalhouse.com <vinicius@digitalhouse.com>
2019-05-16 22:42:06 +0000

    Mumuki Export on 2019-05-16 22:42:06 +0000

--- 00005_Array com números/description.md
+++ 00005_Array com números/description.md
@@ -1,5 +1,5 @@
 A ideia agora é adicionar valores números em uma array. Já sabemos criar uma variavel do tipo array vazia, então ja temos 50% resolvido!
 
-> Novamente crie uma array chamada `$minhaArray` e adicione 3 numero dentro dela.
+> Novamente crie uma array chamada `$minhaArray` e adicione 3 numeros dentro dela.
 
 Se você não lembra qual a sintaxe para agregar valores experimente olhar o campo de dica :point_down: :stuck_out_tongue_winking_eye:
--- 00005_Array com números/test.php
+++ 00005_Array com números/test.php
@@ -5,9 +5,9 @@ public function testResultado(): void {
   $this->assertTrue(is_array($minhaArray), 'A váriavel $minhaArray não é um array!');
   $this->assertTrue(count($minhaArray) == 3, 'A array $minhaArray não contem 3 elementos!');
   
-  $definidoVacio = strpos($sol, '[]');
-  $agregado = strpos($sol, '$miArray[]');
-  $condicionFinal = $definidoVacio !== false && $agregado !== false;
+  $definidoVazio = strpos($solucaoAluno, '[]');
+  $agregado = strpos($solucaoAluno, '$miArray[]');
+  $condicicaoFinal = $definidoVazio !== false && $agregado !== false;
   
   $this->assertTrue($condicionFinal, "Lembre-se você deve agregar valores na array!");
 }

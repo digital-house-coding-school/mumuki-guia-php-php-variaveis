04b3e743232532a17702c6fcfedce209800e06cf
vinicius@digitalhouse.com <vinicius@digitalhouse.com>
2019-05-16 22:37:58 +0000

    Mumuki Export on 2019-05-16 22:37:58 +0000

--- 00003_Tipos de dados/test.php
+++ 00003_Tipos de dados/test.php
@@ -3,13 +3,13 @@ public function testResultado(): void {
   $solucaoAluno = '/*...content...*/';
   
   $vars = get_defined_vars();
-  $estaNulo = array_key_exists("miNulo", $vars);
+  $estaNulo = array_key_exists("$nulo", $vars);
   
   $this->assertTrue(isset($texto), 'A váriavel $texto não está definida.');
   $this->assertTrue(isset($numeroInteiro), 'A váriavel $numeroInteiro não está definida.');
   $this->assertTrue(isset($numeroDecimal), 'A váriavel $numeroDecimal não está definida.');
   $this->assertTrue(isset($verdade), 'A váriavel $verdade não está definida.');
-  $this->assertTrue(isset($nulo), 'A váriavel $nulo não está definida.');
+  $this->assertTrue(isset($estaNulo), 'A váriavel $nulo não está definida.');
   
   $this->assertTrue(gettype($texto) == "string", 'A váriavel $texto deve ser um tipo String');
   $this->assertTrue(gettype($numeroInteiro) == "integer", 'A váriavel $numeroInteiro deve ser um tipo Inteiro');

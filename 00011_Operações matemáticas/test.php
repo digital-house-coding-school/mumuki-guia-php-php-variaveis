abcd549c7d874534f1e8b37096b21afa06270f68
vinicius@digitalhouse.com <vinicius@digitalhouse.com>
2019-05-23 02:48:40 +0000

    Mumuki Export on 2019-05-23 02:48:40 +0000

--- 00011_Operações matemáticas/test.php
+++ 00011_Operações matemáticas/test.php
@@ -2,7 +2,7 @@ public function testResultado(): void {
  
   /*...content...*/
   $sol = '/*...content...*/';
-  $this->assertTrue(isset($soma) && $soma === 125, 'A variavel $soma não foi criada ou não contem o valor correto!');
+  $this->assertTrue(isset($soma) && $soma == 125, 'A variavel $soma não foi criada ou não contem o valor correto!');
   $this->assertTrue(isset($subtração) && $subtração === 75, 'A variavel $subtração não foi criada ou não contem o valor correto!');
   $this->assertTrue(isset($multiplicação) && $multiplicação === 2500, 'A variavel $multiplicação não foi criada ou não contem o valor correto!');
   $this->assertTrue(isset($divisão) && $divisão === 4, 'A variavel $multiplicação não foi criada ou não contem o valor correto!');

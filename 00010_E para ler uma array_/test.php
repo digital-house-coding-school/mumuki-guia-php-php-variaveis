b72f511f960f3106d3478ecd09a804bc9829c2b2
vinicius@digitalhouse.com <vinicius@digitalhouse.com>
2019-05-23 01:05:12 +0000

    Mumuki Export on 2019-05-23 01:05:12 +0000

--- 00010_E para ler uma array_/test.php
+++ 00010_E para ler uma array_/test.php
@@ -2,7 +2,7 @@ public function testResultado(): void {
     /*...content...*/
   $sol = '/*...content...*/';
   
-  $nomeOK = isset($nome) && $nome == "Vinicius";
+  $nomeOK = $nome == "Vinicius";
 
   $this->assertTrue($nomeOK, 'A variavel $nome não foi definda ou está com o valor errado!');
   

4309b28b08a51c035aa1bd5356809d3030311021
vinicius@digitalhouse.com <vinicius@digitalhouse.com>
2019-05-23 02:58:04 +0000

    Mumuki Export on 2019-05-23 02:58:04 +0000

--- 00011_Operações matemáticas/description.md
+++ 00011_Operações matemáticas/description.md
@@ -5,7 +5,7 @@ $numero2 = 25;`
 Seu desafio é construir 5 novas variáveis a partir dela:
 
 > 1. A variável `$soma` que tem a soma de ambas as variáveis
-> 2. A variável `$subtração` que tem o resultado de subtrair `$numero1` de `$numero2` (o resultado deve ser 75)
-> 3. A variável `$multiplicação` que tem o resultado de multiplicar ambos os números.
-> 4. A variável `$divisão` que tem o resultado de dividir o `$numero1` pelo `$numero2` (o resultado deve ser 4)
+> 2. A variável `$subtracao` que tem o resultado de subtrair `$numero1` de `$numero2` (o resultado deve ser 75)
+> 3. A variável `$multiplicacao` que tem o resultado de multiplicar ambos os números.
+> 4. A variável `$divisao` que tem o resultado de dividir o `$numero1` pelo `$numero2` (o resultado deve ser 4)
 > 5. A variavel `$resto` deve conter o resto da divisão do `$numero1` pelo` $numero2` (o resultado deve ser 0)
--- 00011_Operações matemáticas/test.php
+++ 00011_Operações matemáticas/test.php
@@ -3,8 +3,8 @@ public function testResultado(): void {
   /*...content...*/
   $sol = '/*...content...*/';
   $this->assertTrue(isset($soma) && $soma == 125, 'A variavel $soma não foi criada ou não contem o valor correto!');
-  $this->assertTrue(isset($subtração) && $subtração === 75, 'A variavel $subtração não foi criada ou não contem o valor correto!');
-  $this->assertTrue(isset($multiplicação) && $multiplicação === 2500, 'A variavel $multiplicação não foi criada ou não contem o valor correto!');
-  $this->assertTrue(isset($divisão) && $divisão === 4, 'A variavel $multiplicação não foi criada ou não contem o valor correto!');
+  $this->assertTrue(isset($subtracao) && $subtração === 75, 'A variavel $subtração não foi criada ou não contem o valor correto!');
+  $this->assertTrue(isset($multiplicacao) && $multiplicação === 2500, 'A variavel $multiplicação não foi criada ou não contem o valor correto!');
+  $this->assertTrue(isset($divisao) && $divisão === 4, 'A variavel $multiplicação não foi criada ou não contem o valor correto!');
   $this->assertTrue(isset($resto) && $resto === 0, 'A variavel $resto não foi criada ou não contem o valor correto!');
 }

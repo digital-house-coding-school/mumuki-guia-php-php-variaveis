698be3ed95c1cc20d03e4af785607c7bca38349f
vinicius@digitalhouse.com <vinicius@digitalhouse.com>
2019-05-23 03:31:55 +0000

    Mumuki Export on 2019-05-23 03:31:55 +0000

--- 00010_E para ler uma array_/test.php
+++ 00010_E para ler uma array_/test.php
@@ -2,16 +2,8 @@ public function testResultado(): void {
     /*...content...*/
     $sol = '/*...content...*/';
   
-  $erros = [
-      "nome"=> (isset($nome) && is_string($nome) == "Vinicius"),
-      "sobrenome" => (isset($sobrenome)&&$sobrenome == $pessoa['sobrenome']),
-      "idade" => (isset($idade)&&$idade == $pessoa['idade'])
-  ];
-
-
-foreach($erros as $key=>$value){
-   $this->assertTrue($value,"A variavel $key não está definida ou o valor está incorreto!");
-  }
+  $tNome = isset($nome) && $nome === "Vinicius";
   
+   $this->assertTrue($tNome, 'A variavel $nome não foi criada ou está com valor diferente da array!');
 
 }

d06e02c578d7528489092ff7ec710c41adbb3dda
vinicius@digitalhouse.com <vinicius@digitalhouse.com>
2019-05-23 02:14:02 +0000

    Mumuki Export on 2019-05-23 02:14:02 +0000

--- 00008_A batiarray/test.php
+++ 00008_A batiarray/test.php
@@ -13,7 +13,7 @@ public function testDescriptionExample(): void {
   $apellido = array_pop($batman);
   $nombre = array_pop($batman);
   
-  $nombreCorrecto = is_string($nombre) == "Bruce";
+  $nombreCorrecto = $nombre == "Bruce";
   
   $this->assertTrue($nombreCorrecto, 'O nome do Batman não está correto ou não está na primeira posição.');
   

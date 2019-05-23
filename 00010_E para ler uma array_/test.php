5f3bdb8d028793ee8cfe320a646a133087a3e9d9
vinicius@digitalhouse.com <vinicius@digitalhouse.com>
2019-05-23 00:36:37 +0000

    Mumuki Export on 2019-05-23 00:36:37 +0000

--- 00010_E para ler uma array_/test.php
+++ 00010_E para ler uma array_/test.php
@@ -4,11 +4,12 @@ public function testResultado(): void {
   
   $solNome=isset($nome) && $nome == $pessoa['nome'];
   $solSobrenome = isset($sobrenome) && $sobrenome == $pessoa['sobrenome'];
-  $idade = isset($idade) && $idade == $pessoa['idade'];
+  $solIdade = isset($idade) && $idade == $pessoa['idade'];
   
   $this->assertTrue($solNome, 'A variavel $nome não está definida ou o valor está incorreto!');
+  $this->assertTrue($solSobrenome, 'A variavel $sobrenome não está definida ou o valor está incorreto!');
+  $this->assertTrue($solIdade, 'A variavel $sobrenome não está definida ou o valor está incorreto!');
   
- 
-    
-   
+  
+  $this->assertFalse(substr_count($sol, "$pessoa['nome']) == 1, "Lamentamos informar que algo parecería estar...raro en tu código. Las palabras 'Emanuel', 'Manu' y 'Spurs' no deberían aparecer más de una vez en el código enviado...");
 }

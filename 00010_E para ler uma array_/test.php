1e0a1e5f2ac4b044b25cf10639efa13daf99994f
vinicius@digitalhouse.com <vinicius@digitalhouse.com>
2019-05-23 03:34:41 +0000

    Mumuki Export on 2019-05-23 03:34:41 +0000

--- 00010_E para ler uma array_/test.php
+++ 00010_E para ler uma array_/test.php
@@ -2,7 +2,7 @@ public function testResultado(): void {
     /*...content...*/
     $sol = '/*...content...*/';
   
-  $tNome = isset($nome) && $nome === "Vinicius";
+  $tNome = isset($nome) && substr_count($sol, '$nome= $pessoa["nome"]')>0;
   
    $this->assertTrue($tNome, 'A variavel $nome não foi criada ou está com valor diferente da array!');
 

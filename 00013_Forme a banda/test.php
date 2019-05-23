a0cdb83fe34e3bf345a72b821e396d0fc47b9fc8
vinicius@digitalhouse.com <vinicius@digitalhouse.com>
2019-05-23 03:17:07 +0000

    Mumuki Export on 2019-05-23 03:17:07 +0000

--- 00013_Forme a banda/test.php
+++ 00013_Forme a banda/test.php
@@ -1,4 +1,5 @@
 public function testDescriptionExample(): void {
+/*...content...*/
 
   $this->assertTrue(isset($resultado) && $resultado == "Sandy & Junior", "Algo parece errado, verifique se foi usado o sinal para concatenar as informações e espaços foram usados!");
 }

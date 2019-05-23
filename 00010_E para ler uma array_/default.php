c3b8c1a43194a54e3f0bcd8d9733dc7c8a66357b
vinicius@digitalhouse.com <vinicius@digitalhouse.com>
2019-05-23 03:20:23 +0000

    Mumuki Export on 2019-05-23 03:20:23 +0000

--- 00010_E para ler uma array_/default.php
+++ 00010_E para ler uma array_/default.php
@@ -1,11 +1,1 @@
-$pessoa = [
-  "nome"=>"Vinicius",
-  "sobrenome" => "Oliveira",
-  "altura" => 1.78,
-  "idade" => 21,
-  "hobbies" => [
-      "Músicas",
-      "Games",
-      "Comer muita comida!"
-  ]
-];
+$pessoa = ["nome"=>"Vinicius","sobrenome" => "Oliveira","altura" => 1.78,"idade" => 21,"hobbies" => ["Músicas","Games","Comer muita comida!"]];

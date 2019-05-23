c655570f498a34309ef4d981a631f4853e54d732
vinicius@digitalhouse.com <vinicius@digitalhouse.com>
2019-05-23 03:43:32 +0000

    Mumuki Export on 2019-05-23 03:43:32 +0000

--- 00010_E para ler uma array_/test.php
+++ 00010_E para ler uma array_/test.php
@@ -4,6 +4,7 @@ public function testResultado(): void {
     
   $this->assertTrue(isset($pessoa), 'A array $pessoa não foi criada');
    $this->assertTrue(count($pessoa) == 3, 'A array $pessoa não contem as 3 informações');
+   
   $tNome = $pessoa[0];
   $tSobre = $pessoa[1];
   $tIdade = $pessoa[2];

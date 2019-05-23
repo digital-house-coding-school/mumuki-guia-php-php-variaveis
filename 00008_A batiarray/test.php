d0ec85086df9b16dd8ed2dfd7945da146ed30358
vinicius@digitalhouse.com <vinicius@digitalhouse.com>
2019-05-23 02:18:53 +0000

    Mumuki Export on 2019-05-23 02:18:53 +0000

--- 00008_A batiarray/test.php
+++ 00008_A batiarray/test.php
@@ -1,21 +1,6 @@
 public function testDescriptionExample(): void {
   /*...content...*/
   
-
- $this->assertTrue(isset($batman), 'A Variavel $batman não está definida!');
-  $this->assertTrue(is_array($batman), 'A Variavel $batman não é um array!');
-  
- $this->assertTrue(count($batman) == 5, 'A Variavel $batman não contem 5 itens!');
-
-  $cinturon = array_pop($batman);
-  $altura = array_pop($batman);
-  $peso = array_pop($batman);
-  $apellido = array_pop($batman);
-  $nombre = array_pop($batman);
-  var_dump($nombre);
-  $nombreCorrecto = $nombre == "Bruce";
-  
-  
-  
+x
   
 }
